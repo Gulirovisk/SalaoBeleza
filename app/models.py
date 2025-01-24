8ba81e21f0669ffee223edd4939dd9e4b4ee1bff
Gulirovisk <lucinhogarciacaboverde@gmail.com>
2025-01-24 17:20:32 -0300

    Adiciona ícone SVG e limita o tamanho dos campos de descrição nos modelos Profissional e Serviço

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Profissional (models.Model):
     nome = models.CharField(max_length=100, verbose_name="Nome do Profissional")
     especialidade = models.CharField(max_length=100, verbose_name="Especialidade")
-    descricao = models.TextField(verbose_name="Descrição do Profissional", blank=True, null=True)
+    descricao = models.TextField(max_length=255, verbose_name="Descrição do Profissional", blank=True, null=True)
     telefone_whatsapp = models.CharField(max_length=16, verbose_name="WhatsApp", unique=True)
     foto = models.ImageField(upload_to='profissionais', verbose_name="Foto do Profissional", blank=True, null=True)
 
@@ -26,7 +26,7 @@ class Profissional (models.Model):
 class Servico (models.Model):
     nome = models.CharField(max_length=100, verbose_name="Nome do Serviço")
     profissional = models.ForeignKey(Profissional, on_delete=models.CASCADE, verbose_name="Profissional")
-    descricao = models.TextField(verbose_name="Descrição do Serviço", blank=True, null=True)
+    descricao = models.TextField(max_length=255, verbose_name="Descrição do Serviço", blank=True, null=True)
     horario_inicio = models.TimeField(max_length=100, verbose_name="Horário início de Atendimento", blank=True, null=True)
     horario_fim = models.TimeField(max_length=100, verbose_name="Horário término de Atendimento", blank=True, null=True)
     imagem = models.ImageField(upload_to='servicos', verbose_name="Imagem do Serviço", blank=True, null=True)
